a9bf6d59dd9ee02073fd4f018f72c99d1e5f7670
aditya <aditya26.upadhyay@gmail.com>
2025-09-06 11:38:53 +0530

    Fix redirect path in netlify.toml and improve serverless function loading

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,7 +1,24 @@
 const serverless = require('serverless-http');
 
-// Import the main server app
-const app = require('../../server');
+// Try different require paths based on build environment
+let app;
+try {
+  // Try relative path from functions directory
+  app = require('../../server');
+} catch (error) {
+  try {
+    // Try absolute path from project root
+    app = require('/opt/build/repo/server');
+  } catch (error2) {
+    try {
+      // Try another common Netlify path
+      app = require('./../../server');
+    } catch (error3) {
+      console.error('Failed to load server:', error3);
+      throw error3;
+    }
+  }
+}
 
 // Export the serverless handler
-module.exports.handler = serverless(app);
+exports.handler = serverless(app);
